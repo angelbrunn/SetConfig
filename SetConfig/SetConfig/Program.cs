6f62192ac7f3ffb58c9a0f0150de5991fdcdff69
Angel Prado <angelbrunn@gmail.com>
2017-09-07 00:38:54 -0300

    I00-01-AGPB
    I00-01-Creacion del SetConfig - Configuraciones iniciales

--- SetConfig/SetConfig/Program.cs
+++ SetConfig/SetConfig/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.AccessControl;
+using System.Security.Permissions;
 
 namespace SetConfig
 {
@@ -12,7 +14,7 @@ namespace SetConfig
     {
         public static String servidor;
         public static String machine;
-
+        public static Boolean isSave = false;
         static void Main(string[] args)
         {
             //CREA LA ESTRUCTURA DEL DIRECTORIO
@@ -26,7 +28,7 @@ namespace SetConfig
                 Console.WriteLine("---------|Bach SIS-Atlantida|--------");
                 Console.WriteLine("-------------------------------------");
                 Console.WriteLine("[ 0 ] Servidor/Server");
-                Console.WriteLine("[ 1 ] Maquina/Machine");
+                Console.WriteLine("[ 1 ] ServidorSQL/ServerSQL");
                 Console.WriteLine("[ 2 ] Guardar/Save");
                 Console.WriteLine("[ 3 ] Iniciar/Initialize");
                 Console.WriteLine("[ 4 ] Salir/Exit");
@@ -45,6 +47,8 @@ namespace SetConfig
                         if (!string.IsNullOrEmpty(servidor) && !string.IsNullOrEmpty(servidor))
                         {
                             saveFiles(servidor, machine);
+                            isSave = true;
+                            Console.WriteLine("Se configuro nombre de servidorSQL y nombre de Maquina. Inicie!");
                         }
                         else
                         {
@@ -54,8 +58,15 @@ namespace SetConfig
                     case 3:
                         if (!string.IsNullOrEmpty(servidor) && !string.IsNullOrEmpty(servidor))
                         {
-                            ExecuteScriptDB();
-                            moveAppFileConfig();
+                            if (isSave == true)
+                            {
+                                ExecuteScriptDB();
+                                moveAppFileConfig();
+                                deleteDirectory();
+                            }
+                            else {
+                                Console.WriteLine("Falta guardar las configuraciones.");
+                            }
                         }
                         else {
                             Console.WriteLine("Falta configurar nombre de servidorSQL y nombre de Maquina.");
@@ -86,7 +97,7 @@ namespace SetConfig
         {
             //SAVE DB FILE
             // Compose a string that consists of three lines.
-            string sqlcommand = "SQLCMD -S "+ machine + "\\" + servidor + " -E -i script_generacion_Atlantida_db.sql";
+            string sqlcommand = "SQLCMD -S " + machine + " -E -i script_generacion_Atlantida_db.sql";
 
             // Write the string to a file.
             System.IO.StreamWriter dbFile = new System.IO.StreamWriter("c:\\SIS_AtlantidaConfig\\InstallerDB.bat");
@@ -97,51 +108,35 @@ namespace SetConfig
             //SAVE APP.CONFIG FILE
             // Compose a string that consists of three lines.
             String providerName = "providerName =\"System.Data.SqlClient\" ";
-            String name = "name = \"AtlantidaDev\"/>";
+            String name = "name =\"AtlantidaDev\"/>";
 
+            string appConfigh0 = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
+            string appConfigh1 = "<configuration>";
             string appConfig1 = "<connectionStrings>";
-            string appConfig2 = "<add connectionString=Data Source = " + machine + "\\"+ Servidor + "; Initial Catalog = AtlantidaDev; Integrated Security = True " + providerName + " " + name;
+            string appConfig2 = "<add connectionString=\"Data Source=" + machine + ";Initial Catalog=AtlantidaDev;Integrated Security=True \"" + providerName + " " + name;
             string appConfig3 = "</connectionStrings>";
+            string appConfig4 = "</configuration>";
 
             // Write the string to a file.
-            System.IO.StreamWriter appFile = new System.IO.StreamWriter("c:\\SIS_AtlantidaConfig\\data\\App.config");
+            System.IO.StreamWriter appFile = new System.IO.StreamWriter("c:\\SIS_AtlantidaConfig\\data\\Atlantida.UI.exe.config");
+            appFile.WriteLine(appConfigh0);
+            appFile.WriteLine(appConfigh1);
             appFile.WriteLine(appConfig1);
             appFile.WriteLine(appConfig2);
             appFile.WriteLine(appConfig3);
+            appFile.WriteLine(appConfig4);
 
             appFile.Close();
         }
 
-        static void moveAppFileConfig()
-        {
-        }
-
         static void ExecuteScriptDB()
         {
-            int exitCode;
-            ProcessStartInfo processInfo;
-            Process process;
 
-            processInfo = new ProcessStartInfo("cmd.exe");
-            processInfo.CreateNoWindow = true;
-            processInfo.UseShellExecute = false;
-            // *** Redirect the output ***
-            processInfo.RedirectStandardError = true;
-            processInfo.RedirectStandardOutput = true;
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = "c:\\SIS_AtlantidaConfig\\InstallerDB.bat";
+            process.StartInfo.WorkingDirectory = "C:\\SIS_AtlantidaConfig";
+            process.Start();
 
-            process = Process.Start(processInfo);
-            process.WaitForExit();
-
-            // *** Read the streams ***
-            // Warning: This approach can lead to deadlocks, see Edit #2
-            string output = process.StandardOutput.ReadToEnd();
-            string error = process.StandardError.ReadToEnd();
-
-            exitCode = process.ExitCode;
-
-            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
-            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
-            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
             process.Close();
         }
 
@@ -173,10 +168,14 @@ namespace SetConfig
             finally { }
         }
 
+        static void deleteDirectory() {
+            Directory.Delete(@"C:\SIS_AtlantidaConfig", true);
+        }
+
         static void getScriptExecuter()
         {
             string fileName = "script_generacion_Atlantida_db.sql";
-            string sourcePath = @"C:\lugardeinstalacion\a\b";
+            string sourcePath = @"C:\Program Files (x86)\SISAtlantida\Atlantida\script";
             string targetPath = @"C:\SIS_AtlantidaConfig";
 
             // Use Path class to manipulate file and directory paths.
@@ -215,11 +214,73 @@ namespace SetConfig
             }
             else
             {
-                Console.WriteLine("La direccion mensionada no existe!");
+                Console.WriteLine("La direccion mencionada no existe!");
             }
 
             // Keep console window open in debug mode.
-            Console.WriteLine("presione cualquier tecla para salir.");
+            Console.WriteLine("presione cualquier tecla para continuar con la configuracion.");
+            Console.ReadKey();
+        }
+
+        static void moveAppFileConfig()
+        {
+            string fileName = "Atlantida.UI.exe.config";
+            string sourcePath = @"C:\SIS_AtlantidaConfig\data";
+            string targetPath = @"C:\Program Files (x86)\SISAtlantida\Atlantida";
+
+            // Use Path class to manipulate file and directory paths.
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+            // To copy a folder's contents to a new location:
+            // Create a new target folder, if necessary.
+            if (!System.IO.Directory.Exists(targetPath))
+            {
+                System.IO.Directory.CreateDirectory(targetPath);
+            }
+
+            // To copy a file to another location and 
+            // overwrite the destination file if it already exists.
+
+            //FileInfo file1 = new FileInfo(@"C:\Program Files (x86)\SISAtlantida\Atlantida\script\Atlantida.UI.exe.config");
+            // FileInfo file2 = new FileInfo(@"C:\SIS_AtlantidaConfig\data\script\Atlantida.UI.exe.config");
+            // FileSecurity ac1 = file1.GetAccessControl();
+            //ac1.SetAccessRuleProtection(true, true);
+            //file2.SetAccessControl(ac1);
+            string fileA = targetPath + "\\" + fileName;
+            FileSecurity securityA = File.GetAccessControl(fileA);
+
+            string fileB = sourcePath + "\\" + fileName;
+            File.SetAccessControl(fileB, securityA);
+
+            System.IO.File.Copy(fileB, fileA, true);
+
+            // To copy all the files in one directory to another directory.
+            // Get the files in the source folder. (To recursively iterate through
+            // all subfolders under the current directory, see
+            // "How to: Iterate Through a Directory Tree.")
+            // Note: Check for target path was performed previously
+            //       in this code example.
+            if (System.IO.Directory.Exists(sourcePath))
+            {
+                string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                // Copy the files and overwrite destination files if they already exist.
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    fileName = System.IO.Path.GetFileName(s);
+                    destFile = System.IO.Path.Combine(targetPath, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("La direccion mencionada no existe!");
+            }
+
+            // Keep console window open in debug mode.
+            Console.WriteLine("presione cualquier tecla para continuar con la configuracion.");
             Console.ReadKey();
         }
     }
